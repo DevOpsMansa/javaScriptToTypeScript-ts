13c742781551084f551497e9b5fbcc52ed1bfb2f
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-02-20 06:58:33 -0600

    Solved part 1 - Added type annotations for properties in the Vehicle class. Added a union type for the status property in the Vehicle class. Adjusted the Car and MotorCycle classes to match the constructor signature of the Vehicle class. Changed the printStatus function parameter type to Vehicle. Corrected the method name in console.log(myHarley.make.toUpperCase()); to use toUpperCase() instead of toUppercase(). Corrected the property name in console.log(myBuick.model); to use the correct property name model.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+
 # Working with TypeScript
+
 This lab through a list of TypeScript problems to create a TypeScript program that includes classes, unions, literal types, generics, and more. This work through is intended to get familiarize with basic TypeScript syntax and techniques, while also providing insights into the similarities between TypeScript and standard JavaScript code.
 
 ## Objectives
@@ -6,6 +8,55 @@ Convert an existing JavaScript file into TypeScript.
 Fix existing but unidentified bugs in JavaScript code using TypeScript's editor integration. Write TypeScript code from scratch, including:
 Classes (with Generics). Methods and Functions. Variables.
 Test TypeScript code through simple implementations and basic console logging.
+
+Given js code:
+
+class Vehicle {
+  status = "stopped";
+
+  constructor(make, model, wheels) {
+    this.make = make;
+    this.model = model;
+    this.wheels = wheels;
+  }
+  start() {
+    this.status = "started";
+  }
+  stop() {
+    this.status = "stopped";
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model) {
+    super(make, model, "four");
+  }
+}
+
+class MotorCycle extends Vehicle {
+  constructor(make, model) {
+    super(make, model, 2);
+  }
+}
+
+function printStatus(vehicle) {
+  if (vehicle.status === "running") {
+    console.log("The vehicle is running.");
+  } else {
+    console.log("The vehicle is stopped.");
+  }
+}
+
+const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
+myHarley.start();
+printStatus(myHarley);
+console.log(myHarley.make.toUppercase());
+
+const myBuick = new Car("Buick", "Regal");
+myBuick.wheels = myBuick.wheels - 1;
+console.log(myBuick.wheels);
+console.log(myBuick.mdl);
+
 
 
 ## [] Part 1: Converting JavaScript to TypeScript
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,51 @@
 // @ts-check
 
+/**
+ * Added type annotations for properties in the Vehicle class.
 class Vehicle {
   status = "stopped";
+*/
+  class Vehicle {
+    status: "started" | "stopped" = "stopped";
+    make: string;
+    model: string;
+    wheels: number;
 
+/**
+ *Added a union type for the status property in the Vehicle class.
   constructor(make, model, wheels) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
   }
-  start() {
+*/
+  constructor(make: string, model: string, wheels: number | string) {
+    this.make = make;
+    this.model = model;
+    this.wheels = typeof wheels === "number" ? wheels : parseInt(wheels);
+  }
+/**
+ * added : void to status check
+ *  start() {
     this.status = "started";
   }
   stop() {
     this.status = "stopped";
   }
 }
+ */
+ 
+start(): void {
+  this.status = "started";
+}
+
+stop(): void {
+  this.status = "stopped";
+}
+}
+
+/**
+ * Adjusted the Car and MotorCycle classes to match the constructor signature of the Vehicle class.
 
 class Car extends Vehicle {
   constructor(make, model) {
@@ -27,7 +58,23 @@ class MotorCycle extends Vehicle {
     super(make, model, 2);
   }
 }
+*/
+class Car extends Vehicle {
+  constructor(make: string, model: string) {
+    super(make, model, 4);
+  }
+}
 
+class MotorCycle extends Vehicle {
+  constructor(make: string, model: string) {
+    super(make, model, 2);
+  }
+}
+
+/**
+ * 
+ * @param vehicle Changed the printStatus function parameter type to Vehicle.
+ * 
 function printStatus(vehicle) {
   if (vehicle.status === "running") {
     console.log("The vehicle is running.");
@@ -35,13 +82,36 @@ function printStatus(vehicle) {
     console.log("The vehicle is stopped.");
   }
 }
+ */
+function printStatus(vehicle: Vehicle): void {
+  if (vehicle.status === "started") {
+    console.log("The vehicle is running.");
+  } else {
+    console.log("The vehicle is stopped.");
+  }
+}
 
+/**
+ * Corrected the method name in console.log(myHarley.make.toUpperCase()); to use toUpperCase() instead of toUppercase().
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
 console.log(myHarley.make.toUppercase());
+*/
+const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
+myHarley.start();
+printStatus(myHarley);
+console.log(myHarley.make.toUpperCase()); // Note the corrected method name "toUpperCase"
 
+/**
+ * Corrected the property name in console.log(myBuick.model); to use the correct property name "model".
+ 
 const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.mdl);
+*/
+const myBuick = new Car("Buick", "Regal");
+myBuick.wheels = myBuick.wheels - 1;
+console.log(myBuick.wheels);
+console.log(myBuick.model); // Note the corrected property name "model"
